266f9bbd0dacd1ba3eafe0eec21a9a706385a8f9
pys <bark6256@naver.com>
2022-12-13 17:15:11 +0900

    mysql을 이용한 블로그 프로젝트 시작

---
+++ src/main/java/com/cos/blogmysql/BlogmysqlApplication.java
@@ -0,0 +1,13 @@
+package com.cos.blogmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogmysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogmysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cos/blogmysql/BlogmysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cos.blogmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogmysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
