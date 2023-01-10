6937dbf4144a9fb02be92edd4a44cb70e4a7074b
pys <bark6256@naver.com>
2023-01-10 19:09:49 +0900

    id(주소)로 select

--- src/main/java/com/cos/blogmysql/test/BlogRestControllerTest.java
+++ src/main/java/com/cos/blogmysql/test/BlogRestControllerTest.java
@@ -1,6 +1,10 @@
 package com.cos.blogmysql.test;
 
+import java.util.function.Supplier;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +30,13 @@ public class BlogRestControllerTest {
 		user.setRole(RoleType.USER);
 		userRepository.save(user);
 		return "회원가입 완료";
+	}
+	
+	@GetMapping("/dummy/user/{id}")
+	public User detail(@PathVariable int id) {
+		User user = userRepository.findById(id).orElseThrow(() -> {
+			return new IllegalArgumentException("해당 유저는 없습니다. id : " + id);
+		});
+		return user;
 	}
 }
