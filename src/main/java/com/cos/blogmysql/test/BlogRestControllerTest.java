cf13fa8884ed67f6e0c761445fb5af0ebd4ba78b
pys <bark6256@naver.com>
2023-01-13 17:26:30 +0900

    전체 select, 페이징 select

--- src/main/java/com/cos/blogmysql/test/BlogRestControllerTest.java
+++ src/main/java/com/cos/blogmysql/test/BlogRestControllerTest.java
@@ -1,8 +1,12 @@
 package com.cos.blogmysql.test;
 
-import java.util.function.Supplier;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,5 +42,17 @@ public class BlogRestControllerTest {
 			return new IllegalArgumentException("해당 유저는 없습니다. id : " + id);
 		});
 		return user;
+	}
+	
+	@GetMapping("/dumm/users")
+	public List<User> list(){
+		return userRepository.findAll();
+	}
+	
+	@GetMapping("/dummy/user/page/{page}")
+	public List<User> pageList(@PageableDefault(size = 2, sort = "id", direction = Sort.Direction.DESC) Pageable pageable){
+		Page<User> pageUsers = userRepository.findAll(pageable);
+		List<User> users = pageUsers.getContent();
+		return users;
 	}
 }
