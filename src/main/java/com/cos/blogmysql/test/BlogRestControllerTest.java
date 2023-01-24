a160c7a2ca90258ecb434f2760c68b381910451d
pys <bark6256@naver.com>
2023-01-24 18:07:00 +0900

    더티체킹을 이용한 업데이트

--- src/main/java/com/cos/blogmysql/test/BlogRestControllerTest.java
+++ src/main/java/com/cos/blogmysql/test/BlogRestControllerTest.java
@@ -7,9 +7,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.blogmysql.model.RoleType;
@@ -54,5 +57,16 @@ public class BlogRestControllerTest {
 		Page<User> pageUsers = userRepository.findAll(pageable);
 		List<User> users = pageUsers.getContent();
 		return users;
+	}
+	
+	@Transactional
+	@PutMapping("/dummy/user/{id}")
+	public User updateUser(@PathVariable int id, @RequestBody User requestUser) {
+		User user = userRepository.findById(id).orElseThrow(()->{
+			return new IllegalArgumentException("해당 유저는 없습니다. id : " + id);
+		});
+		user.setPassword(requestUser.getPassword());
+		user.setEmail(requestUser.getEmail());
+		return null;
 	}
 }
