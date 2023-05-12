3a268afbf39bb1eca50921038a9b4444e2973bb7
pys <bark6256@naver.com>
2023-05-12 17:10:09 +0900

    로그인, 회원가입 화면 작성

--- src/main/java/com/cos/blogmysql/controller/UserController.java
+++ src/main/java/com/cos/blogmysql/controller/UserController.java
@@ -1,5 +1,20 @@
 package com.cos.blogmysql.controller;
 
-public class UserController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class UserController {
+	
+	@GetMapping("/user/joinForm")
+	public String joinFrom() {
+		
+		return "user/joinForm";
+	}
+	
+	@GetMapping("/user/loginForm")
+	public String loginFrom() {
+		
+		return "user/loginForm";
+	}
 }
